3fec3871ce8295d2f6650357af00f60a78d1eeb1
tkdwns414 <sangjune2000@naver.com>
2023-11-16 22:36:47 +0900

    initial commit

---
+++ src/main/java/org/millie/www/MillieServer/MillieServerApplication.java
@@ -0,0 +1,13 @@
+package org.millie.www.MillieServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillieServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/millie/www/MillieServer/MillieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.millie.www.MillieServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillieServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
