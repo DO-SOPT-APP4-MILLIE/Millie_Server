47405f157292620efd935a91ac0a0a17db6cc850
tkdwns414 <sangjune2000@naver.com>
2023-11-27 03:53:28 +0900

    feature/8: add findByIdOrThrow query in repository

--- src/main/java/org/millie/www/MillieServer/book/repository/BookJpaRepository.java
+++ src/main/java/org/millie/www/MillieServer/book/repository/BookJpaRepository.java
@@ -1,7 +1,13 @@
 package org.millie.www.MillieServer.book.repository;
 
 import org.millie.www.MillieServer.book.domain.Book;
+import org.millie.www.MillieServer.common.exception.BusinessException;
+import org.millie.www.MillieServer.common.httpmessage.ExceptionMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookJpaRepository extends JpaRepository<Book, Long> {
+    default Book findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new BusinessException(ExceptionMessage.BOOK_NOT_FOUND_EXCEPTION));
+    }
 }
