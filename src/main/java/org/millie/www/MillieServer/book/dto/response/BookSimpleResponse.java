bf57182bf7974c917876ee08862e1c84d41130fd
tkdwns414 <sangjune2000@naver.com>
2023-11-27 03:36:22 +0900

    feature/7: create BookSimpleResponse dto

---
+++ src/main/java/org/millie/www/MillieServer/book/dto/response/BookSimpleResponse.java
@@ -0,0 +1,17 @@
+package org.millie.www.MillieServer.book.dto.response;
+
+import org.millie.www.MillieServer.book.domain.Book;
+
+public record BookSimpleResponse(
+        Long id, String title, String thumbnail, String author, Integer completionRate, Integer readingTime){
+    public static BookSimpleResponse of(Book book) {
+        return new BookSimpleResponse(
+                book.getId(),
+                book.getTitle(),
+                book.getImageUrl(),
+                book.getAuthor(),
+                book.getCompletionRate(),
+                book.getReadingTime()
+        );
+    }
+}
